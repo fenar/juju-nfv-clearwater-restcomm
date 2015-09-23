56d6599a1871af017c8d25142af7c5e3c1216c63
Nicolas Thomas <nicolas.thomas@canonical.com>
2015-09-23 12:27:23 +0200

    Temporary using a branch for DNS trusty (pending merge) will revert to official if test passed.

--- 00-prepare.sh
+++ 00-prepare.sh
@@ -2,6 +2,10 @@
 
 set -e
 echo "Getting the code from Clearwater project until direct from git deploy is possible"
-
+UP=$PWD
 [  -d clearwater-juju ]  && (cd clearwater-juju; git pull)
 [  -d clearwater-juju ]  || git clone -b dnsaas https://github.com/Metaswitch/clearwater-juju.git
+cd $UP
+# Temporary until merge in upstream
+[ -d clearwater-juju/charms/trusty/dns ] && (cd clearwater-juju/charms/trusty/dns; git pull)
+[ -d clearwater-juju/charms/trusty/dns ] || (mkdir -p clearwater-juju/charms/trusty/dns; git clone  https://github.com/thomnico/DNS-Charm.git clearwater-juju/charms/trusty/dns)
