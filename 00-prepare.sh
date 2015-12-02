b59f05c098fe9d8a7f76135c3c3c8e0b9e918f4e
Nicolas Thomas <nicolas.thomas@canonical.com>
2015-12-02 11:40:22 +0100

    Switch to using the upstream branches of projects again.

--- 00-prepare.sh
+++ 00-prepare.sh
@@ -4,8 +4,10 @@ set -e
 echo "Getting the code from Clearwater project until direct from git deploy is possible"
 UP=$PWD
 [  -d clearwater-juju ]  && (cd clearwater-juju; git pull)
-[  -d clearwater-juju ]  || git clone -b dns_json https://github.com/thomnico/clearwater-juju-thomnico.git clearwater-juju
+[  -d clearwater-juju ]  || git clone -b dns_json https://github.com/Metaswitch/clearwater-juju.git clearwater-juju
 cd $UP
 # Temporary until merge in upstream
 [ -d clearwater-juju/charms/trusty/dns ] && (cd clearwater-juju/charms/trusty/dns; git pull)
-[ -d clearwater-juju/charms/trusty/dns ] || (mkdir -p clearwater-juju/charms/trusty/dns; git clone  https://github.com/thomnico/DNS-Charm.git clearwater-juju/charms/trusty/dns)
+[ -d clearwater-juju/charms/trusty/dns ] || (mkdir -p clearwater-juju/charms/trusty/dns; git clone  https://github.com/chuckbutler/DNS-Charm.git clearwater-juju/charms/trusty/dns)
+
+echo "Code should be up to date. If source git changed you must rm -rf clearwater-juju before running this script"
