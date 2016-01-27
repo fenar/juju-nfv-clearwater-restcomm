d64a2be134c0ca59668db4a7f1c5b472ac7b51fa
Nicolas Thomas <nicolas.thomas@canonical.com>
2016-01-27 11:42:47 +0100

    Use the nogit branch for charms ..

--- 00-prepare.sh
+++ 00-prepare.sh
@@ -4,7 +4,7 @@ set -e
 echo "Getting the code from Clearwater project until direct from git deploy is possible"
 UP=$PWD
 [  -d clearwater-juju ]  && (cd clearwater-juju; git pull)
-[  -d clearwater-juju ]  || git clone -b trusty https://github.com/thomnico/clearwater-juju.git clearwater-juju
+[  -d clearwater-juju ]  || git clone -b nogit https://github.com/thomnico/clearwater-juju.git clearwater-juju
 cd $UP
 # Temporary until merge in upstream
 [ -d clearwater-juju/charms/trusty/dns ] && (cd clearwater-juju/charms/trusty/dns; git pull)
