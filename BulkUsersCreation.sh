63ce098be01500b615b819783aa6d87829bdf3ef
Nicolas Thomas <nicolas.thomas@canonical.com>
2016-04-19 13:43:00 -0500

    Update to trusty

--- BulkUsersCreation.sh
+++ BulkUsersCreation.sh
@@ -4,15 +4,12 @@
 
 juju ssh clearwater-homestead/0 '. /etc/clearwater/config; for DN in {0000..0099} ; do echo sip:$DN@$home_domain,$DN@$home_domain,$home_domain,7kkzTyGW ; done > users.csv'
 
-juju ssh clearwater-homestead/0 "/usr/share/clearwater/homestead/src/metaswitch/crest/tools/bulk_autocomplete.py users.csv"
-juju ssh clearwater-homestead/0 "/usr/share/clearwater/homestead/src/metaswitch/crest/tools/bulk_create.py users.auto.csv"
+#juju ssh clearwater-homestead/0 "/usr/share/clearwater/homestead/src/metaswitch/crest/tools/bulk_autocomplete.py users.csv"
+#juju ssh clearwater-homestead/0 "/usr/share/clearwater/homestead/src/metaswitch/crest/tools/bulk_create.py users.auto.csv"
+juju ssh clearwater-homestead/0 "/usr/share/clearwater/crest/src/metaswitch/crest/tools/bulk_autocomplete.py users.csv"
+juju ssh clearwater-homestead/0 "/usr/share/clearwater/crest/src/metaswitch/crest/tools/bulk_autocomplete.py users.auto.csv"
 
-juju ssh clearwater-homestead/0 "./users.auto.create_homestead.sh"
 
-TMP=`mktemp -d`
-juju scp clearwater-homestead/0:~/*xdm* $TMP/
-juju scp $TMP/*xdm* clearwater-homer/0:~/ 
-juju ssh clearwater-homer/0 "./users.auto.create_xdm.sh"
 
 DOMAIN=`juju ssh  clearwater-homestead/0 '. /etc/clearwater/config; echo $home_domain'`
 echo 'users range from 0000@'$DOMAIN
