1abe11d4d23cdccf69744ef64ce6074a7a96df95
Nicolas Thomas <nicolas.thomas@canonical.com>
2016-08-23 10:05:54 +0200

    Some updates/workaround and try to use the "action"

--- BulkUsersCreation.sh
+++ BulkUsersCreation.sh
@@ -1,18 +1,12 @@
 #!/bin/bash -e
 
-## Try ping homer and homestead from ellis before adding user to check dns resolution
 
-juju ssh clearwater-homestead/0 '. /etc/clearwater/config; for DN in {0000..0099} ; do echo sip:$DN@$home_domain,$DN@$home_domain,$home_domain,7kkzTyGW ; done > users.csv'
+# Try ping homer and homestead from ellis before adding user to check dns resolution
 
-#juju ssh clearwater-homestead/0 "/usr/share/clearwater/homestead/src/metaswitch/crest/tools/bulk_autocomplete.py users.csv"
-#juju ssh clearwater-homestead/0 "/usr/share/clearwater/homestead/src/metaswitch/crest/tools/bulk_create.py users.auto.csv"
-juju ssh clearwater-homestead/0 "/usr/share/clearwater/crest/src/metaswitch/crest/tools/bulk_autocomplete.py users.csv"
-juju ssh clearwater-homestead/0 "/usr/share/clearwater/crest/src/metaswitch/crest/tools/bulk_autocomplete.py users.auto.csv"
-
-
+for DN in {0000..0009}; do juju action do clearwater-homestead/0 create-update-user number=\"$DN\" password=7kkzTyGW; done
 
 DOMAIN=`juju ssh  clearwater-homestead/0 '. /etc/clearwater/config; echo $home_domain'`
 echo 'users range from 0000@'$DOMAIN
-echo 'to  0099@'$DOMAIN
+echo 'to  0019@'$DOMAIN
 echo ' with password 7kkzTyGW created'
 echo "See clearwater doc to configure your clients"
